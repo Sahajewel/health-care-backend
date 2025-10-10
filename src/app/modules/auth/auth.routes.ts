d941be46076512c09a5a3f1d10b4a3c2f2ef5150
Saha-Jewel <jewelsaha072@gmail.com>
2025-10-10 21:50:02 +0900

    added login

---
+++ src/app/modules/auth/auth.routes.ts
@@ -0,0 +1,7 @@
+import express, { NextFunction, Request, Response } from 'express'
+import { AuthControllers } from './auth.controller';
+
+const router = express.Router();
+
+router.post("/login", AuthControllers.login)
+export const AuthRoutes = router
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { UserRoutes } from '../modules/users/user.routes';
+import { AuthRoutes } from '../modules/auth/auth.routes';
 
 
 const router = express.Router();
@@ -8,7 +9,11 @@ const moduleRoutes = [
     {
         path: '/user',
         route: UserRoutes
-    }
+    },
+    {
+        path: '/auth',
+        route: AuthRoutes
+    },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route))
