1b33365f1d9d97756adec3d9dfa5113eab9b2e95
Saha-Jewel <jewelsaha072@gmail.com>
2025-10-11 00:42:23 +0900

    jwt token created

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,5 +11,8 @@ export default {
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KRY,
         api_secret: process.env.CLOUDINARY_API_SECRET,
-    }
+    },
+    jwt_secret:process.env.JWT_SECRET,
+    jwt_access_expires: process.env.JWT_ACCESS_EXPIRES,
+    jwt_refresh_expires: process.env.JWT_REFRESH_EXPIRES,
 }
